8e6238f36bbc3669c56ae7a96db2ec76301b4c4f
Yahyobek1505 <yahyohonorinboyev0105@gmail.com>
2023-12-10 01:06:28 +0500

    Homework is half done.

---
+++ script.js
@@ -0,0 +1,34 @@
+// Alisherning chet el safari========>
+// let borPul = +prompt("Bor pulingizni kiriting");
+// //Dollar kursi========>
+// let dollarKursi =11000;
+//  //Euro kursi========>
+// let euroKursi =12354;
+// // Xarajatlar========>
+// let $bilet = 500*11000;
+// let $hotel = 250*11000;
+// let $muzey = 120*12354;
+// //$Jami xarajat======>
+// let $xarajat =$bilet+$hotel+$muzey;
+// // hisob-kitob
+// let result =borPul - $xarajat;
+// if (result >=0 ) {
+// alert("Oq yo'l Alisher");
+//   console.log("Oq yo'l Alisher");
+// } else {
+//   alert("Ozgina sabr qil");
+//   console.log("Ozgina sabr qil");
+// }
+// Alisherning chet el safari========>
+
+/**Masalalar */
+// 1-masala======>
+let numberOne = prompt("Birinchi sonni kiriting");
+let numberTwo = prompt("Ikkinchi sonni kiriting");
+let numberThree = prompt("Uchinchi sonni kiriting");
+// Tekshirish
+if (numberOne > numberTwo){
+  
+}else {
+
+}
